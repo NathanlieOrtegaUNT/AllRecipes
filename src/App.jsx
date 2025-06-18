9866e4f236b7bbf24626649793e287cb731fd290
Kobra Amiri <kobraAmiri@my.unt.edu>
2025-06-17 20:17:04 -0500

    Hey i have added one line code here

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 // AllRecipes/src/App.jsx
 
 import { FavoritesProvider } from './context/FavoritesContext';
+import { ThemeProvider } from './context/ThemeContext';
 import './App.css';
+import './components/layout/toggle-switch.css';
 import Pages from './pages/Pages';
 
 function App() {
   return (
-    <FavoritesProvider>
-      <div className="app-container"> 
-        <Pages />
-      </div>
-    </FavoritesProvider>
+    <ThemeProvider>
+      <FavoritesProvider>
+        <div className="app-container"> 
+          <Pages />
+        </div>
+      </FavoritesProvider>
+    </ThemeProvider>
   )
 }
 
