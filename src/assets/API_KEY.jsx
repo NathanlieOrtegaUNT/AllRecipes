bb9f0b6ddc6b44107cb4b6fb907da08f043a948f
NathanlieOrtega <NathanlieOrtega.Dev@gmail.com>
2025-07-27 03:53:07 -0500

    Updated API KEY to ensure safety push.

--- src/assets/API_KEY.jsx
+++ src/assets/API_KEY.jsx
@@ -1,3 +1,3 @@
-// export const API_KEY = '0790ddd262c24c55825e9141d9a472d9';
-export const API_KEY = '81bdc134fb73435fbb14311ed16cb557';
-// export const API_KEY = '0790ddd262c24c55825e9141d9a472d9'
+// src/API_KEY.jsx
+
+export const API_KEY = import.meta.env.VITE_RECIPE_API_KEY;
