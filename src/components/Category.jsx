579b9acc115be2064b03d9961095a8ab366f31a8
NathanlieOrtega <NathanlieOrtega.Dev@gmail.com>
2025-07-27 03:48:49 -0500

    Renovated the API design of this project

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,24 +1,47 @@
 import { FaPizzaSlice, FaHamburger } from 'react-icons/fa';
 import { GiNoodles, GiChopsticks } from 'react-icons/gi';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate, useParams } from 'react-router-dom';
 import './Category.css'
 
 const Category = () => {
+    const navigate = useNavigate();
+    const { type } = useParams();
+
+    const handleCategoryClick = (cuisine, e) => {
+
+        if (type === cuisine) {
+            e.preventDefault();
+            navigate('/');
+        }
+    };
+
     return (
         <div className="category-container">
-            <NavLink to={'/cuisine/Italian'}>
+            <NavLink 
+                to={'/cuisine/Italian'}
+                onClick={(e) => handleCategoryClick('Italian', e)}
+            >
                 <FaPizzaSlice />
                 <p>Italian</p>
             </NavLink>
-            <NavLink to={'/cuisine/American'}>
+            <NavLink 
+                to={'/cuisine/American'}
+                onClick={(e) => handleCategoryClick('American', e)}
+            >
                 <FaHamburger />
                 <p>American</p>
             </NavLink>
-            <NavLink to={'/cuisine/Thai'}>
+            <NavLink 
+                to={'/cuisine/Thai'}
+                onClick={(e) => handleCategoryClick('Thai', e)}
+            >
                 <GiNoodles />
                 <p>Thai</p>
             </NavLink>
-            <NavLink to={'/cuisine/Chinese'}>
+            <NavLink 
+                to={'/cuisine/Chinese'}
+                onClick={(e) => handleCategoryClick('Chinese', e)}
+            >
                 <GiChopsticks />
                 <p>Chinese</p>
             </NavLink>
