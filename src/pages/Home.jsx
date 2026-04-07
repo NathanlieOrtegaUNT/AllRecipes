944c0371348447764839e2a4d7d60d2c04a803bb
Nathanlie Ortega <nathanlieortega.dev@gmail.com>
2026-04-07 02:58:53 -0500

    Revert "recently viewed feature"
    This reverts commit 1c7b5c1ea1ae446dc09403b1d294e627ecf3c3ed.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,10 @@
-/// kobra's code 
-
-import RecentlyViewed from "../components/RecentlyViewed";
-
-
 import Veggie from "../components/Veggie";
 import Popular from "../components/Popular";
 import './Home.css';
 
 const Home = () => {
-
-/// kobra's code 
     return (
         <div className="home-container">
-        <RecentlyViewed />
             <Popular />
             <Veggie />
         </div>
